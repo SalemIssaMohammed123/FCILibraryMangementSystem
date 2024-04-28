b64552336bd166784bb5d42f1f282aeb0980e68b
salem <salmy7270@gmail.com>
2024-04-29 02:56:20 +0300

    Hello Everyone, This My project in the MVC

---
+++ Controllers/ProfileController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authorization.Infrastructure;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using Test.Models;
+using Test.ViewModels;
+namespace Test.Controllers
+{
+    public class ProfileController : Controller
+    {
+        private readonly UserManager<ApplicationUser> userManager;
+
+        ProfileController(UserManager<ApplicationUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+        [Authorize]
+        public async Task<IActionResult> Index()
+        {
+            Claim UserClaim= User.Claims.FirstOrDefault(c=>c.Type==ClaimTypes.NameIdentifier);
+            string id= UserClaim.Value.ToString();
+            ApplicationUser user= await userManager.FindByIdAsync(id);
+            ProfileViewModel myProfile= new ProfileViewModel();
+            myProfile.UserName= user.UserName;
+            myProfile.ImgUrl = user.ImageUrl;
+            return PartialView(myProfile);
+        }
+    }
+}
---
+++ ViewModels/ProfileViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Test.ViewModels
+{
+    public class ProfileViewModel
+    {
+        public string UserName;
+
+        public string ImgUrl;
+
+    }
+}
