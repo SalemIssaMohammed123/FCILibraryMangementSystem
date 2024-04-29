c80359032b495e4c40d1d696a1f3528f41fe0bc4
salem <salmy7270@gmail.com>
2024-04-29 06:42:49 +0300

    FCI_LibraryMangementSystemDigramCreated

---
+++ Migrations/20240429031718_final.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Test.Migrations
+{
+    public partial class final : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Report");
+
+            migrationBuilder.RenameColumn(
+                name: "Password_hint",
+                table: "AspNetUsers",
+                newName: "ImageUrl");
+
+            migrationBuilder.CreateTable(
+                name: "BorrowBook",
+                columns: table => new
+                {
+                    BorrowBookID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    BorrowedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ReturnDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    expired = table.Column<bool>(type: "bit", nullable: false),
+                    BookId = table.Column<int>(type: "int", nullable: false),
+                    ApplicationUserId = table.Column<string>(type: "nvarchar(450)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BorrowBook", x => x.BorrowBookID);
+                    table.ForeignKey(
+                        name: "FK_BorrowBook_AspNetUsers_ApplicationUserId",
+                        column: x => x.ApplicationUserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_BorrowBook_Books_BookId",
+                        column: x => x.BookId,
+                        principalTable: "Books",
+                        principalColumn: "BookID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ReportForBook",
+                columns: table => new
+                {
+                    ReportBookID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    report = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    BookId = table.Column<int>(type: "int", nullable: false),
+                    ApplicationUserId = table.Column<string>(type: "nvarchar(450)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ReportForBook", x => x.ReportBookID);
+                    table.ForeignKey(
+                        name: "FK_ReportForBook_AspNetUsers_ApplicationUserId",
+                        column: x => x.ApplicationUserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ReportForBook_Books_BookId",
+                        column: x => x.BookId,
+                        principalTable: "Books",
+                        principalColumn: "BookID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BorrowBook_ApplicationUserId",
+                table: "BorrowBook",
+                column: "ApplicationUserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BorrowBook_BookId",
+                table: "BorrowBook",
+                column: "BookId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ReportForBook_ApplicationUserId",
+                table: "ReportForBook",
+                column: "ApplicationUserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ReportForBook_BookId",
+                table: "ReportForBook",
+                column: "BookId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BorrowBook");
+
+            migrationBuilder.DropTable(
+                name: "ReportForBook");
+
+            migrationBuilder.RenameColumn(
+                name: "ImageUrl",
+                table: "AspNetUsers",
+                newName: "Password_hint");
+
+            migrationBuilder.CreateTable(
+                name: "Report",
+                columns: table => new
+                {
+                    ReportId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    book_id = table.Column<int>(type: "int", nullable: false),
+                    report = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Report", x => x.ReportId);
+                    table.ForeignKey(
+                        name: "FK_Report_Books_book_id",
+                        column: x => x.book_id,
+                        principalTable: "Books",
+                        principalColumn: "BookID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Report_book_id",
+                table: "Report",
+                column: "book_id");
+        }
+    }
+}
